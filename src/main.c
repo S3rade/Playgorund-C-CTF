07fb75032227d83c8ed6a4a5ee9b9ab31be32dcd
Zachary Phoon <73163517+S3rade@users.noreply.github.com>
2024-10-16 13:48:00 +0800

    Updated so that the flags are auto generated

--- src/main.c
+++ src/main.c
@@ -1,44 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "../include/game.h"
+
+// Function prototypes
+void clear_input_buffer();
+char *generate_flag(int length);
 
 int main() {
     char choice;
     do {
         // Define questions and answers
         const char *questions[][3] = {
-            {"How to print a line in C?", "printf(\"Hello, World!\");","5bSwsGc7Uh09T1EktIJa5ASIludLTa"}, 
-            {"How to declare a variable(x) in C?", "int x;","tyGOrn7AfsXftkljWu3HowzCglCKDq"}, 
-            {"How to read user input in C?", "scanf(\"%d\", &x);","rzzM9E1Tv83G3LlUCqnQQFlRnRP7ci"},
-            {"How to declare a function in C?", "void my_function();","zBVHrQj5BvhtdBN4MVtc87GiWz6jFo"},
-            {"How to define a function in C?", "void my_function();","kU1NyqhAFZojC0AdrT02f7VNZrsPcD"},
-            {"How to call a function in C?", "my_function();","kJn6yDS7LDdBZWhgxku8aDmc1wYP7c"},
-            {"How to write a comment in C?", "// This is a comment","6hl4COluzdgMX3YUz1yviRjTYy0r0k"},
-            {"How to declare a constant (x) in C?", "const int x = 10;","TtVYV22Eq6V8ibANgJ8c6ueBmMMoFP"},
-            {"How to declare a string in C?", "char str[] = \"Hello\";","xSRGKL2FZqGfmH8xTgMJMC96BJ0t8j"},
-            {"How to declare a character (A) in C?", "char c = 'A';","mixoNXlshfKbyIxPNVVKrap0Miedby"},
-            {"How to declare a float in C? Float = 3.14", "float f = 3.14;","wWmrEx0fM4i6wMeorPAwR4RyeaXPTY"},
-            {"How to declare a double in C? d = 3.14", "double d = 3.14;","F8ERECTetnXQSmE75UtOeofFNFaRRx"},
-            {"How to declare an array of 5 in C?", "int arr[5];","1oTJWCAv84bZ5hsnU0RBbyIWyf1uBK"},
-            {"How to declare a pointer in C?", "int *ptr;","ihWkspOZwouRckNLyP77BUSIUtcARa"},
-            {"How to use a pointer in C?", "ptr = &x;","NREIUO3025k55HO7Wp2ffTRvBOAGBT"},
-            {"How to declare a structure in C?", "struct MyStruct { int x; };","EHC32877brE7mb7kpzfFD4iejfiEHJ"},
-            {"How to declare an enum in C?", "enum MyEnum { A, B, C };","oML5A2xW4dGhnvib5Q9XNBcTN0E8ki"},
-            {"How to declare a union in C?", "union MyUnion { int x; float y; };","I7YwJlA7APVHlLcspZhx3gwjOstFma"},
-            {"How to use if statement in C?", "if (x > 0) { printf(\"Positive\"); }","rdfoUFZyQndqGS6nQ9DnftiTf859ZD"},
-            {"How to use else statement in C?", "if (x > 0) { printf(\"Positive\"); } else { printf(\"Negative\"); }","bl2A5jOOD1QRWttaMHIhpQQdK8sN1G"},
-            {"How to use else if statement in C?", "if (x > 0) { printf(\"Positive\"); } else if (x < 0) { printf(\"Negative\"); }","ijenLrfluZ8VVrqyWxPjarKUn3Rkkp"},
-            {"How to use switch statement in C?", "switch (x) { case 1: printf(\"One\"); break; case 2: printf(\"Two\"); break; default: printf(\"Other\"); }","qSAi11RTIzXq9q0v3dXUM9BiVvacyR"},
-            {"How to use while loop in C?", "while (x > 0) { printf(\"%d\", x); x--; }","32taHAEN3GrrCTe98pNqtpQX0EGC3A"},
-            {"How to use do while loop in C?", "do { printf(\"%d\", x); x--; } while (x > 0);","ajArytkrRePcF8D89sHb1JN9V0hUkI"},
-            {"How to use for loop in C?", "for (int i = 0; i < 5; i++) { printf(\"%d\", i); }","QsExB4fMBxtCN86oJcE3St1Nw1xQ1t"},
-            {"How to use break statement in C?", "for (int i = 0; i < 5; i++) { if (i == 3) { break; } printf(\"%d\", i); }","mt4kGE89ki0Mo9SqS7VSMVH1qKvKfs"},
-            {"How to use continue statement in C?", "for (int i = 0; i < 5; i++) { if (i == 3) { continue; } printf(\"%d\", i); }","wBzts0LhH4jhl7SxS5qTGFrbW6w2l0"},
-            {"How to use goto statement in C?", "int i = 0; loop: printf(\"%d\", i);","Wp7EguuspijbCRFYFbhL4Zh8tWeIF9"},
+            {"How to print a line in C?", "printf(\"Hello, World!\");",""}, 
+            {"How to declare a variable(x) in C?", "int x;",""}, 
+            {"How to read user input in C?", "scanf(\"%d\", &x);",""},
+            {"How to declare a function in C?", "void my_function();",""},
+            {"How to define a function in C?", "void my_function();",""},
+            {"How to call a function in C?", "my_function();",""},
+            {"How to write a comment in C?", "// This is a comment",""},
+            {"How to declare a constant (x) in C?", "const int x = 10;",""},
+            {"How to declare a string in C?", "char str[] = \"Hello\";",""},
+            {"How to declare a character (A) in C?", "char c = 'A';",""},
+            {"How to declare a float in C? Float = 3.14", "float f = 3.14;",""},
+            {"How to declare a double in C? d = 3.14", "double d = 3.14;",""},
+            {"How to declare an array of 5 in C?", "int arr[5];",""},
+            {"How to declare a pointer in C?", "int *ptr;",""},
+            {"How to use a pointer in C?", "ptr = &x;",""},
+            {"How to declare a structure in C?", "struct MyStruct { int x; };",""},
+            {"How to declare an enum in C?", "enum MyEnum { A, B, C };",""},
+            {"How to declare a union in C?", "union MyUnion { int x; float y; };",""},
+            {"How to use if statement in C?", "if (x > 0) { printf(\"Positive\"); }",""},
+            {"How to use else statement in C?", "if (x > 0) { printf(\"Positive\"); } else { printf(\"Negative\"); }",""},
+            {"How to use else if statement in C?", "if (x > 0) { printf(\"Positive\"); } else if (x < 0) { printf(\"Negative\"); }",""},
+            {"How to use switch statement in C?", "switch (x) { case 1: printf(\"One\"); break; case 2: printf(\"Two\"); break; default: printf(\"Other\"); }",""},
+            {"How to use while loop in C?", "while (x > 0) { printf(\"%d\", x); x--; }",""},
+            {"How to use do while loop in C?", "do { printf(\"%d\", x); x--; } while (x > 0);",""},
+            {"How to use for loop in C?", "for (int i = 0; i < 5; i++) { printf(\"%d\", i); }",""},
+            {"How to use break statement in C?", "for (int i = 0; i < 5; i++) { if (i == 3) { break; } printf(\"%d\", i); }",""},
+            {"How to use continue statement in C?", "for (int i = 0; i < 5; i++) { if (i == 3) { continue; } printf(\"%d\", i); }",""},
+            {"How to use goto statement in C?", "int i = 0; loop: printf(\"%d\", i);",""},
 
         };
         
         int num_questions = sizeof(questions) / sizeof(questions[0]);
         int score = 0; // Initialize score
+
+        // Seed the random number generator
+        srand(time(NULL));
+
+        // Generate flags for each question
+        for (int i = 0; i < num_questions; i++) {
+            // Generate a flag based on the length of the question
+            questions[i][2] = generate_flag(strlen(questions[i][0]));
+        }
 
         // Loop through each question
         for (int i = 0; i < num_questions; i++) {
@@ -64,3 +80,20 @@ void clear_input_buffer() {
     while (getchar() != '\n');
 }
 
+// Function to generate a flag of a given length
+char *generate_flag(int length) {
+    // Allocate memory for the flag (+1 for the null terminator)
+    char *flag = malloc((length + 1) * sizeof(char));
+    if (flag == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Generate a random alphanumeric string
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (int i = 0; i < length; i++) {
+        flag[i] = charset[rand() % (sizeof(charset) - 1)];
+    }
+    flag[length] = '\0'; // Null-terminate the string
+    return flag;
+}
